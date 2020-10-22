1c1bb8157f051028b8c54e58422867eb5048e569
Sérgio Moura <smouracalmon@gmail.com>
2020-10-22 16:10:05 -0300

    Modele Post criada

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+// Criar e exportar uma função
+// Essa função recebe dois parametros: sequelize, DataTypes
+// A função retorna um objeto que é definido a partir do sequelize.define
+
+module.exports = (sequelize, DataTypes) => {
+
+    const post = sequelize.define(
+        'Post',
+        {
+            texto: DataTypes.STRING(100),
+            img: DataTypes.STRING(100),
+            usuarios_id: DataTypes.INTEGER,
+            n_likes: DataTypes.INTEGER
+        },
+        {
+            tableName: 'posts',
+            timestamps: false
+        }
+    );
+
+    post.associate = (models) =>{
+        post.belongsTo(models.Usuario,{as:'usuario', foreignKey:'usuarios_id'});
+    }
+
+    return post;
+
+}
--- testes/testeModelPost.js
+++ testes/testeModelPost.js
@@ -1,8 +1,14 @@
 const { sequelize, Post } = require('../models');
 
-Post.findAll().then(
-    data => {
-        console.log(data.map( u => u.toJSON()));
-        sequelize.close();
+// Post.findAll().then(
+//     data => {
+//         console.log(data.map( u => u.toJSON()));
+//         sequelize.close();
+//     }
+// )
+
+Post.findByPk(2,{include:'usuario'}).then(
+    post => {
+        console.log(post.toJSON());
     }
 )
